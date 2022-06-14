571e396c432e71f2fba96e02c93b7b2547d6f50b
Usn Zorro <>
2022-06-14 21:34:41 +0300

    Revert "Remove dead code"
    This reverts commit 965e46b6c62b027fed3bd778797983d2bb9d0f31.

--- src/treasury/ref_finance.rs
+++ src/treasury/ref_finance.rs
@@ -24,12 +24,22 @@ trait RefFinance {
 
     fn get_pool_shares(&self, pool_id: u64, account_id: AccountId) -> U128;
 
+    fn predict_remove_liquidity(&self, pool_id: u64, shares: U128) -> Vec<U128>;
+
     #[payable]
     fn add_stable_liquidity(&mut self, pool_id: u64, amounts: Vec<U128>, min_shares: U128) -> U128;
 
     #[payable]
     fn remove_liquidity(&mut self, pool_id: u64, shares: U128, min_amounts: Vec<U128>)
         -> Vec<U128>;
+
+    #[payable]
+    fn remove_liquidity_by_tokens(
+        &mut self,
+        pool_id: u64,
+        amounts: Vec<U128>,
+        max_burn_shares: U128,
+    ) -> U128;
 
     #[payable]
     fn withdraw(&mut self, token_id: AccountId, amount: U128, unregister: Option<bool>);
